8e08901423480c1e5df7171d199523195a7d92ee
Jesus Aguilar <jesusalejandro.aguilar.conde@gmail.com>
2022-10-29 14:47:56 -0500

    Final version para preentrega 3

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$('.carrusel').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    centermode: true,
+    arrows: false,
+    mobileFirst: true,
+    pauseOnHover: false,
+  });
